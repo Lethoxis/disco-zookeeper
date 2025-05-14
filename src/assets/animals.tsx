e881856ac441e4fc6013b89bbffe91010dabff03
Lethoxis <louis.braillon@hotmail.fr>
2025-05-14 13:34:11 +0200

    All animals & regions images

--- src/assets/animals.tsx
+++ src/assets/animals.tsx
@@ -1,13 +1,18 @@
+import * as Rarity from "./animalRarities";
+
 type AnimalCoord = Array<number>;
 
 export type Animal = {
   name: string;
+  displayName?: string;
+  rarity: Rarity.AnimalRarity;
   coords: Array<AnimalCoord>;
 };
 
-// Farm
-export const Sheep = {
+//================== Farm ==================
+export const Sheep: Animal = {
   name: "Sheep",
+  rarity: Rarity.Common,
   coords: [
     [0, 0],
     [0, 1],
@@ -16,8 +21,9 @@ export const Sheep = {
   ],
 };
 
-export const Rabbit = {
+export const Rabbit: Animal = {
   name: "Rabbit",
+  rarity: Rarity.Common,
   coords: [
     [0, 0],
     [1, 0],
@@ -26,8 +32,9 @@ export const Rabbit = {
   ],
 };
 
-export const Pig = {
+export const Pig: Animal = {
   name: "Pig",
+  rarity: Rarity.Common,
   coords: [
     [0, 0],
     [1, 0],
@@ -36,8 +43,9 @@ export const Pig = {
   ],
 };
 
-export const Cow = {
+export const Cow: Animal = {
   name: "Cow",
+  rarity: Rarity.Rare,
   coords: [
     [0, 0],
     [0, 1],
@@ -45,8 +53,9 @@ export const Cow = {
   ],
 };
 
-export const Horse = {
+export const Horse: Animal = {
   name: "Horse",
+  rarity: Rarity.Rare,
   coords: [
     [0, 0],
     [1, 0],
@@ -54,8 +63,9 @@ export const Horse = {
   ],
 };
 
-export const Unicorn = {
+export const Unicorn: Animal = {
   name: "Unicorn",
+  rarity: Rarity.Mythical,
   coords: [
     [0, 0],
     [1, 1],
@@ -63,9 +73,20 @@ export const Unicorn = {
   ],
 };
 
-// Outback
-export const Kangaroo = {
+export const Chicken: Animal = {
+  name: "Chicken",
+  rarity: Rarity.Timeless,
+  coords: [
+    [2, 0],
+    [1, 1],
+    [0, 2],
+  ],
+};
+
+//================== Outback ==================
+export const Kangaroo: Animal = {
   name: "Kangaroo",
+  rarity: Rarity.Common,
   coords: [
     [0, 0],
     [1, 1],
@@ -74,8 +95,9 @@ export const Kangaroo = {
   ],
 };
 
-export const Platypus = {
+export const Platypus: Animal = {
   name: "Platypus",
+  rarity: Rarity.Common,
   coords: [
     [0, 0],
     [0, 1],
@@ -84,8 +106,9 @@ export const Platypus = {
   ],
 };
 
-export const Crocodile = {
+export const Crocodile: Animal = {
   name: "Crocodile",
+  rarity: Rarity.Common,
   coords: [
     [0, 0],
     [0, 1],
@@ -94,8 +117,9 @@ export const Crocodile = {
   ],
 };
 
-export const Koala = {
+export const Koala: Animal = {
   name: "Koala",
+  rarity: Rarity.Rare,
   coords: [
     [0, 0],
     [0, 1],
@@ -103,8 +127,9 @@ export const Koala = {
   ],
 };
 
-export const Cockatoo = {
+export const Cockatoo: Animal = {
   name: "Cockatoo",
+  rarity: Rarity.Rare,
   coords: [
     [0, 0],
     [1, 1],
@@ -112,8 +137,9 @@ export const Cockatoo = {
   ],
 };
 
-export const Tiddalik = {
+export const Tiddalik: Animal = {
   name: "Tiddalik",
+  rarity: Rarity.Mythical,
   coords: [
     [1, 0],
     [0, 1],
@@ -121,9 +147,20 @@ export const Tiddalik = {
   ],
 };
 
-// Savanna
-export const Zebra = {
+export const Echidna: Animal = {
+  name: "Echidna",
+  rarity: Rarity.Timeless,
+  coords: [
+    [1, 0],
+    [1, 1],
+    [0, 2],
+  ],
+};
+
+//================== Savanna ==================
+export const Zebra: Animal = {
   name: "Zebra",
+  rarity: Rarity.Common,
   coords: [
     [0, 1],
     [1, 0],
@@ -132,8 +169,9 @@ export const Zebra = {
   ],
 };
 
-export const Hippo = {
+export const Hippo: Animal = {
   name: "Hippo",
+  rarity: Rarity.Common,
   coords: [
     [0, 0],
     [0, 2],
@@ -142,8 +180,9 @@ export const Hippo = {
   ],
 };
 
-export const Giraffe = {
+export const Giraffe: Animal = {
   name: "Giraffe",
+  rarity: Rarity.Common,
   coords: [
     [0, 0],
     [1, 0],
@@ -152,8 +191,9 @@ export const Giraffe = {
   ],
 };
 
-export const Lion = {
+export const Lion: Animal = {
   name: "Lion",
+  rarity: Rarity.Rare,
   coords: [
     [0, 0],
     [0, 1],
@@ -161,8 +201,9 @@ export const Lion = {
   ],
 };
 
-export const Elephant = {
+export const Elephant: Animal = {
   name: "Elephant",
+  rarity: Rarity.Rare,
   coords: [
     [0, 0],
     [0, 1],
@@ -170,8 +211,9 @@ export const Elephant = {
   ],
 };
 
-export const Gryphon = {
+export const Gryphon: Animal = {
   name: "Gryphon",
+  rarity: Rarity.Mythical,
   coords: [
     [0, 0],
     [1, 1],
@@ -179,19 +221,31 @@ export const Gryphon = {
   ],
 };
 
-// Northern
-export const Bear = {
+export const Rhinoceros: Animal = {
+  name: "Rhinoceros",
+  rarity: Rarity.Timeless,
+  coords: [
+    [0, 1],
+    [1, 0],
+    [2, 1],
+  ],
+};
+
+//================== Northern ==================
+export const Bear: Animal = {
   name: "Bear",
+  rarity: Rarity.Common,
   coords: [
     [0, 0],
     [0, 1],
     [1, 1],
-    [1, 2],
+    [2, 1],
   ],
 };
 
-export const Skunk = {
+export const Skunk: Animal = {
   name: "Skunk",
+  rarity: Rarity.Common,
   coords: [
     [1, 0],
     [1, 1],
@@ -200,8 +254,9 @@ export const Skunk = {
   ],
 };
 
-export const Beaver = {
+export const Beaver: Animal = {
   name: "Beaver",
+  rarity: Rarity.Common,
   coords: [
     [1, 0],
     [1, 1],
@@ -210,8 +265,9 @@ export const Beaver = {
   ],
 };
 
-export const Moose = {
+export const Moose: Animal = {
   name: "Moose",
+  rarity: Rarity.Rare,
   coords: [
     [0, 0],
     [1, 1],
@@ -219,8 +275,9 @@ export const Moose = {
   ],
 };
 
-export const Fox = {
+export const Fox: Animal = {
   name: "Fox",
+  rarity: Rarity.Rare,
   coords: [
     [0, 0],
     [0, 1],
@@ -228,10 +285,843 @@ export const Fox = {
   ],
 };
 
-export const Sasquatch = {
+export const Sasquatch: Animal = {
   name: "Sasquatch",
+  rarity: Rarity.Mythical,
   coords: [
     [0, 0],
     [1, 0],
+  ],
+};
+
+export const Otter: Animal = {
+  name: "Otter",
+  rarity: Rarity.Timeless,
+  coords: [
+    [0, 0],
+    [1, 0],
+    [1, 1],
+  ],
+};
+
+//================== Polar ==================
+export const Penguin: Animal = {
+  name: "Penguin",
+  rarity: Rarity.Common,
+  coords: [
+    [0, 1],
+    [1, 1],
+    [2, 0],
+    [2, 2],
+  ],
+};
+
+export const Muskox: Animal = {
+  name: "Muskox",
+  rarity: Rarity.Common,
+  coords: [
+    [0, 0],
+    [0, 1],
+    [1, 0],
+    [1, 2],
+  ],
+};
+
+export const Seal: Animal = {
+  name: "Seal",
+  rarity: Rarity.Common,
+  coords: [
+    [0, 0],
+    [1, 1],
+    [2, 2],
+    [1, 3],
+  ],
+};
+
+export const PolarBear: Animal = {
+  name: "Polar Bear",
+  rarity: Rarity.Rare,
+  coords: [
+    [0, 0],
+    [0, 2],
+    [1, 2],
+  ],
+};
+
+export const Walrus: Animal = {
+  name: "Walrus",
+  rarity: Rarity.Rare,
+  coords: [
+    [0, 0],
+    [1, 1],
+    [1, 2],
+  ],
+};
+
+export const Yeti: Animal = {
+  name: "Yeti",
+  rarity: Rarity.Mythical,
+  coords: [
+    [0, 0],
+    [2, 0],
+  ],
+};
+
+export const SnowyOwl: Animal = {
+  name: "Snowy Owl",
+  rarity: Rarity.Timeless,
+  coords: [
+    [0, 0],
+    [0, 1],
+    [1, 1],
+  ],
+};
+
+//================== Jungle ==================
+export const Monkey: Animal = {
+  name: "Monkey",
+  rarity: Rarity.Common,
+  coords: [
+    [0, 0],
+    [1, 1],
+    [0, 2],
+    [1, 3],
+  ],
+};
+
+export const Toucan: Animal = {
+  name: "Toucan",
+  rarity: Rarity.Common,
+  coords: [
+    [0, 1],
+    [1, 0],
+    [2, 1],
+    [3, 1],
+  ],
+};
+
+export const Gorilla: Animal = {
+  name: "Gorilla",
+  rarity: Rarity.Common,
+  coords: [
+    [0, 0],
+    [1, 0],
+    [0, 2],
+    [1, 2],
+  ],
+};
+
+export const Panda: Animal = {
+  name: "Panda",
+  rarity: Rarity.Rare,
+  coords: [
+    [1, 0],
+    [0, 2],
+    [2, 2],
+  ],
+};
+
+export const Tiger: Animal = {
+  name: "Tiger",
+  rarity: Rarity.Rare,
+  coords: [
+    [0, 0],
+    [0, 2],
+    [0, 3],
+  ],
+};
+
+export const Phoenix: Animal = {
+  name: "Phoenix",
+  rarity: Rarity.Mythical,
+  coords: [
+    [0, 0],
+    [2, 2],
+  ],
+};
+
+export const Lemur: Animal = {
+  name: "Lemur",
+  rarity: Rarity.Timeless,
+  coords: [
+    [0, 0],
+    [1, 1],
+    [2, 0],
+  ],
+};
+
+//================== Jurassic ==================
+export const Diplodocus: Animal = {
+  name: "Diplodocus",
+  rarity: Rarity.Common,
+  coords: [
+    [0, 0],
+    [1, 1],
+    [1, 2],
+    [2, 1],
+  ],
+};
+
+export const Stegosaurus: Animal = {
+  name: "Stegosaurus",
+  rarity: Rarity.Common,
+  coords: [
+    [1, 0],
+    [0, 1],
+    [0, 2],
+    [1, 3],
+  ],
+};
+
+export const Raptor: Animal = {
+  name: "Raptor",
+  rarity: Rarity.Common,
+  coords: [
+    [0, 0],
+    [0, 1],
+    [1, 1],
+    [2, 2],
+  ],
+};
+
+export const TRex: Animal = {
+  name: "T-Rex",
+  rarity: Rarity.Rare,
+  coords: [
+    [0, 0],
+    [2, 0],
+    [2, 1],
+  ],
+};
+
+export const Triceratops: Animal = {
+  name: "Triceratops",
+  rarity: Rarity.Rare,
+  coords: [
+    [0, 0],
+    [1, 2],
+    [2, 0],
+  ],
+};
+
+export const Dragon: Animal = {
+  name: "Dragon",
+  rarity: Rarity.Mythical,
+  coords: [
+    [0, 0],
+    [1, 2],
+  ],
+};
+
+export const Ankylosaurus: Animal = {
+  name: "Ankylosaurus",
+  rarity: Rarity.Timeless,
+  coords: [
+    [1, 0],
+    [0, 2],
+    [1, 2],
+  ],
+};
+
+//================== Ice Age ==================
+export const WoolyRhino: Animal = {
+  name: "Wooly Rhino",
+  rarity: Rarity.Common,
+  coords: [
+    [1, 0],
+    [2, 1],
+    [0, 2],
+    [1, 3],
+  ],
+};
+
+export const GiantSloth: Animal = {
+  name: "Giant Sloth",
+  rarity: Rarity.Common,
+  coords: [
+    [0, 0],
+    [2, 0],
+    [1, 2],
+    [2, 2],
+  ],
+};
+
+export const DireWolf: Animal = {
+  name: "Dire Wolf",
+  rarity: Rarity.Common,
+  coords: [
+    [1, 0],
+    [0, 1],
+    [2, 1],
+    [1, 3],
+  ],
+};
+
+export const SaberTooth: Animal = {
+  name: "Saber Tooth",
+  rarity: Rarity.Rare,
+  coords: [
+    [0, 0],
+    [1, 2],
+    [2, 1],
+  ],
+};
+
+export const Mammoth: Animal = {
+  name: "Mammoth",
+  rarity: Rarity.Rare,
+  coords: [
+    [0, 1],
+    [1, 0],
+    [2, 2],
+  ],
+};
+
+export const Akhlut: Animal = {
+  name: "Akhlut",
+  rarity: Rarity.Mythical,
+  coords: [
+    [1, 0],
+    [0, 2],
+    [2, 2],
+  ],
+};
+
+export const YukonCamel: Animal = {
+  name: "Yukon Camel",
+  rarity: Rarity.Timeless,
+  coords: [
+    [1, 0],
+    [0, 2],
+    [2, 3],
+  ],
+};
+
+//================== City ==================
+export const Raccoon: Animal = {
+  name: "Raccoon",
+  rarity: Rarity.Common,
+  coords: [
+    [0, 0],
+    [1, 0],
+    [0, 2],
+    [1, 3],
+  ],
+};
+
+export const Pigeon: Animal = {
+  name: "Pigeon",
+  rarity: Rarity.Common,
+  coords: [
+    [0, 0],
+    [1, 1],
+    [2, 1],
+    [2, 2],
+  ],
+};
+
+export const Rat: Animal = {
+  name: "Rat",
+  rarity: Rarity.Common,
+  coords: [
+    [0, 0],
+    [0, 1],
+    [1, 1],
+    [1, 3],
+  ],
+};
+
+export const Squirrel: Animal = {
+  name: "Squirrel",
+  rarity: Rarity.Rare,
+  coords: [
+    [1, 0],
+    [2, 1],
+    [0, 2],
+  ],
+};
+
+export const Opossum: Animal = {
+  name: "Opossum",
+  rarity: Rarity.Rare,
+  coords: [
+    [0, 0],
+    [1, 0],
+    [1, 2],
+  ],
+};
+
+export const SewerTurtle: Animal = {
+  name: "Sewer Turtle",
+  rarity: Rarity.Mythical,
+  coords: [
+    [0, 0],
+    [0, 1],
+  ],
+};
+
+export const Chipmunk: Animal = {
+  name: "Chipmunk",
+  rarity: Rarity.Timeless,
+  coords: [
+    [1, 0],
+    [0, 1],
+    [1, 3],
+  ],
+};
+
+//================== Mountain ==================
+export const Goat: Animal = {
+  name: "Goat",
+  rarity: Rarity.Common,
+  coords: [
+    [0, 0],
+    [1, 0],
+    [1, 1],
+    [1, 2],
+  ],
+};
+
+export const Cougar: Animal = {
+  name: "Cougar",
+  rarity: Rarity.Common,
+  coords: [
+    [0, 0],
+    [1, 1],
+    [2, 2],
+    [2, 0],
+  ],
+};
+
+export const Elk: Animal = {
+  name: "Elk",
+  rarity: Rarity.Common,
+  coords: [
+    [0, 0],
+    [1, 1],
+    [0, 2],
+    [1, 2],
+  ],
+};
+
+export const Eagle: Animal = {
+  name: "Eagle",
+  rarity: Rarity.Rare,
+  coords: [
+    [0, 0],
+    [1, 0],
+    [2, 1],
+  ],
+};
+
+export const Coyote: Animal = {
+  name: "Coyote",
+  rarity: Rarity.Rare,
+  coords: [
+    [0, 0],
+    [0, 1],
+    [1, 2],
+  ],
+};
+
+export const Aatxe: Animal = {
+  name: "Aatxe",
+  rarity: Rarity.Mythical,
+  coords: [
+    [1, 0],
+    [0, 2],
+  ],
+};
+
+export const Pika: Animal = {
+  name: "Pika",
+  rarity: Rarity.Timeless,
+  coords: [
+    [0, 0],
+    [0, 2],
+    [1, 2],
+  ],
+};
+
+//================== Nocturnal ==================
+export const Badger: Animal = {
+  name: "Badger",
+  rarity: Rarity.Common,
+  coords: [
+    [1, 0],
+    [2, 0],
+    [0, 2],
+    [1, 2],
+  ],
+};
+
+export const Bat: Animal = {
+  name: "Bat",
+  rarity: Rarity.Common,
+  coords: [
+    [0, 0],
+    [0, 2],
+    [1, 1],
+    [2, 1],
+  ],
+};
+
+export const Kiwi: Animal = {
+  name: "Kiwi",
+  rarity: Rarity.Common,
+  coords: [
+    [1, 0],
+    [0, 1],
+    [1, 2],
+    [2, 2],
+  ],
+};
+
+export const FlyingSquirrel: Animal = {
+  name: "Flying Squirrel",
+  rarity: Rarity.Rare,
+  coords: [
+    [1, 0],
+    [0, 2],
+    [1, 3],
+  ],
+};
+
+export const Kakapo: Animal = {
+  name: "Kakapo",
+  rarity: Rarity.Rare,
+  coords: [
+    [0, 0],
+    [0, 1],
+    [2, 1],
+  ],
+};
+
+export const Ghost: Animal = {
+  name: "Ghost",
+  rarity: Rarity.Mythical,
+  coords: [
+    [0, 0],
+    [1, 1],
+  ],
+};
+
+export const Firefly: Animal = {
+  name: "Firefly",
+  rarity: Rarity.Timeless,
+  coords: [
+    [0, 0],
+    [1, 0],
+    [2, 2],
+  ],
+};
+
+//================== Moon ==================
+export const Moonkey: Animal = {
+  name: "Moonkey",
+  rarity: Rarity.Common,
+  coords: [
+    [0, 0],
+    [1, 0],
+    [1, 2],
+    [2, 2],
+  ],
+};
+
+export const LunarTick: Animal = {
+  name: "Lunar Tick",
+  rarity: Rarity.Common,
+  coords: [
+    [0, 1],
+    [2, 1],
+    [3, 0],
+    [3, 2],
+  ],
+};
+
+export const Tribble: Animal = {
+  name: "Tribble",
+  rarity: Rarity.Common,
+  coords: [
+    [0, 1],
+    [1, 0],
+    [1, 1],
+    [1, 2],
+  ],
+};
+
+export const Moonicorn: Animal = {
+  name: "Moonicorn",
+  rarity: Rarity.Rare,
+  coords: [
+    [0, 0],
+    [1, 0],
+    [1, 1],
+  ],
+};
+
+export const LunaMoth: Animal = {
+  name: "Luna Moth",
+  rarity: Rarity.Rare,
+  coords: [
+    [0, 0],
+    [2, 1],
+    [0, 2],
+  ],
+};
+
+export const JadeRabbit: Animal = {
+  name: "Jade Rabbit",
+  rarity: Rarity.Mythical,
+  coords: [
+    [0, 0],
+    [2, 1],
+  ],
+};
+
+export const Babmoon: Animal = {
+  name: "Babmoon",
+  rarity: Rarity.Timeless,
+  coords: [
+    [2, 0],
+    [0, 1],
+    [1, 2],
+  ],
+};
+
+//================== Mars ==================
+export const Rock: Animal = {
+  name: "Rock",
+  rarity: Rarity.Common,
+  coords: [
+    [0, 0],
+    [1, 0],
+    [0, 1],
+    [1, 1],
+  ],
+};
+
+export const Marsmot: Animal = {
+  name: "Marsmot",
+  rarity: Rarity.Common,
+  coords: [
+    [0, 1],
+    [1, 1],
+    [2, 1],
+    [2, 0],
+  ],
+};
+
+export const Marsmoset: Animal = {
+  name: "Marsmoset",
+  rarity: Rarity.Common,
+  coords: [
+    [0, 0],
+    [0, 2],
+    [1, 2],
+    [2, 1],
+  ],
+};
+
+export const Rover: Animal = {
+  name: "Rover",
+  rarity: Rarity.Rare,
+  coords: [
+    [1, 0],
+    [0, 1],
+    [1, 2],
+  ],
+};
+
+export const Martian: Animal = {
+  name: "Martian",
+  rarity: Rarity.Rare,
+  coords: [
+    [0, 0],
+    [1, 1],
+    [0, 2],
+  ],
+};
+
+export const Marsmallow: Animal = {
+  name: "Marsmallow",
+  rarity: Rarity.Mythical,
+  coords: [
+    [0, 0],
+    [2, 0],
+  ],
+};
+
+export const Marsten: Animal = {
+  name: "Marsten",
+  rarity: Rarity.Timeless,
+  coords: [
+    [0, 0],
+    [0, 2],
+    [1, 3],
+  ],
+};
+
+//================== Constellation ==================
+export const Chamaeleon: Animal = {
+  name: "Chamaeleon",
+  rarity: Rarity.Common,
+  coords: [
+    [0, 1],
+    [1, 0],
+    [1, 1],
+    [0, 3],
+  ],
+};
+
+export const Corvus: Animal = {
+  name: "Corvus",
+  rarity: Rarity.Common,
+  coords: [
+    [0, 1],
+    [0, 2],
+    [2, 0],
+    [2, 2],
+  ],
+};
+
+export const Lynx: Animal = {
+  name: "Lynx",
+  rarity: Rarity.Common,
+  coords: [
+    [0, 1],
+    [1, 1],
+    [1, 0],
+    [2, 0],
+  ],
+};
+
+export const Pisces: Animal = {
+  name: "Pisces",
+  rarity: Rarity.Rare,
+  coords: [
+    [2, 0],
+    [0, 1],
+    [1, 2],
+  ],
+};
+
+export const Capricornus: Animal = {
+  name: "Capricornus",
+  rarity: Rarity.Rare,
+  coords: [
+    [0, 0],
+    [1, 2],
+    [0, 3],
+  ],
+};
+
+export const Pegasus: Animal = {
+  name: "Pegasus",
+  rarity: Rarity.Mythical,
+  coords: [
+    [2, 0],
+    [0, 2],
+  ],
+};
+
+export const Horologium: Animal = {
+  name: "Horologium",
+  rarity: Rarity.Timeless,
+  coords: [
+    [0, 0],
+    [1, 2],
+    [2, 2],
+  ],
+};
+
+//================== Pets ==================
+export const RabbitPet: Animal = {
+  name: "RabbitPet",
+  displayName: "Rabbit",
+  rarity: Rarity.Pet,
+  coords: [
+    [0, 0],
+    [1, 0],
+    [2, 1],
+    [1, 2],
+    [0, 2],
+  ],
+};
+export const Bird: Animal = {
+  name: "Bird",
+  rarity: Rarity.Pet,
+  coords: [
+    [0, 0],
+    [1, 1],
+    [0, 2],
+    [1, 3],
+    [0, 4],
+  ],
+};
+export const Dog: Animal = {
+  name: "Dog",
+  rarity: Rarity.Pet,
+  coords: [
+    [0, 1],
+    [1, 0],
+    [1, 1],
+    [2, 0],
+    [2, 1],
+  ],
+};
+export const Cat: Animal = {
+  name: "Cat",
+  rarity: Rarity.Pet,
+  coords: [
+    [0, 0],
+    [0, 1],
+    [1, 0],
+    [2, 0],
+    [2, 1],
+  ],
+};
+export const Fish: Animal = {
+  name: "Fish",
+  rarity: Rarity.Pet,
+  coords: [
+    [1, 0],
+    [1, 1],
+    [1, 2],
+    [0, 3],
+    [2, 3],
+  ],
+};
+export const Turtle: Animal = {
+  name: "Turtle",
+  rarity: Rarity.Pet,
+  coords: [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+    [1, 1],
+    [2, 1],
+  ],
+};
+export const Lizard: Animal = {
+  name: "Lizard",
+  rarity: Rarity.Pet,
+  coords: [
+    [0, 0],
+    [1, 0],
+    [2, 0],
+    [2, 1],
+    [2, 2],
+  ],
+};
+export const Hamster: Animal = {
+  name: "Hamster",
+  rarity: Rarity.Pet,
+  coords: [
+    [0, 0],
+    [0, 2],
+    [1, 1],
+    [2, 0],
+    [2, 2],
   ],
 };
--- src/assets/regions.tsx
+++ src/assets/regions.tsx
@@ -3,6 +3,7 @@ import * as A from "./animals";
 export type Region = {
   name: string;
   backgroundColor: string;
+  headerColor: string;
   animals: Array<A.Animal>;
 };
 
@@ -10,11 +11,13 @@ export const regions: Array<Region> = [
   {
     name: "Farm",
     backgroundColor: "#638E35",
-    animals: [A.Sheep, A.Rabbit, A.Pig, A.Cow, A.Horse, A.Unicorn],
+    headerColor: "#76B822",
+    animals: [A.Sheep, A.Rabbit, A.Pig, A.Cow, A.Horse, A.Unicorn, A.Chicken],
   },
   {
     name: "Outback",
     backgroundColor: "#C28F52",
+    headerColor: "#D29065",
     animals: [
       A.Kangaroo,
       A.Platypus,
@@ -22,16 +25,170 @@ export const regions: Array<Region> = [
       A.Koala,
       A.Cockatoo,
       A.Tiddalik,
+      A.Echidna,
     ],
   },
   {
     name: "Savanna",
     backgroundColor: "#A3884F",
-    animals: [A.Zebra, A.Hippo, A.Giraffe, A.Lion, A.Elephant, A.Gryphon],
+    headerColor: "#D9AC45",
+    animals: [
+      A.Zebra,
+      A.Hippo,
+      A.Giraffe,
+      A.Lion,
+      A.Elephant,
+      A.Gryphon,
+      A.Rhinoceros,
+    ],
   },
   {
     name: "Northern",
     backgroundColor: "#59852D",
-    animals: [A.Bear, A.Skunk, A.Beaver, A.Moose, A.Fox, A.Sasquatch],
+    headerColor: "#68974D",
+    animals: [A.Bear, A.Skunk, A.Beaver, A.Moose, A.Fox, A.Sasquatch, A.Otter],
   },
+  {
+    name: "Polar",
+    backgroundColor: "#8FC6CB",
+    headerColor: "#83C0C0",
+    animals: [
+      A.Penguin,
+      A.Muskox,
+      A.Seal,
+      A.PolarBear,
+      A.Walrus,
+      A.Yeti,
+      A.SnowyOwl,
+    ],
+  },
+  {
+    name: "Jungle",
+    backgroundColor: "#1E4F11",
+    headerColor: "#459250",
+    animals: [
+      A.Monkey,
+      A.Toucan,
+      A.Gorilla,
+      A.Panda,
+      A.Tiger,
+      A.Phoenix,
+      A.Lemur,
+    ],
+  },
+  {
+    name: "Jurassic",
+    backgroundColor: "#321104",
+    headerColor: "#874E37",
+    animals: [
+      A.Diplodocus,
+      A.Stegosaurus,
+      A.Raptor,
+      A.TRex,
+      A.Triceratops,
+      A.Dragon,
+      A.Ankylosaurus,
+    ],
+  },
+  {
+    name: "Ice Age",
+    backgroundColor: "#91A19B",
+    headerColor: "#A3BBB0",
+    animals: [
+      A.WoolyRhino,
+      A.GiantSloth,
+      A.DireWolf,
+      A.SaberTooth,
+      A.Mammoth,
+      A.Akhlut,
+      A.YukonCamel,
+    ],
+  },
+  {
+    name: "City",
+    backgroundColor: "#505253",
+    headerColor: "#6E7072",
+    animals: [
+      A.Raccoon,
+      A.Pigeon,
+      A.Rat,
+      A.Squirrel,
+      A.Opossum,
+      A.SewerTurtle,
+      A.Chipmunk,
+    ],
+  },
+  {
+    name: "Mountain",
+    backgroundColor: "#6B7D7B",
+    headerColor: "#84998F",
+    animals: [A.Goat, A.Cougar, A.Elk, A.Eagle, A.Coyote, A.Aatxe, A.Pika],
+  },
+  {
+    name: "Nocturnal",
+    backgroundColor: "#5C7576",
+    headerColor: "#5C7576",
+    animals: [
+      A.Badger,
+      A.Bat,
+      A.Kiwi,
+      A.FlyingSquirrel,
+      A.Kakapo,
+      A.Ghost,
+      A.Firefly,
+    ],
+  },
+  {
+    name: "Moon",
+    backgroundColor: "#64605E",
+    headerColor: "#7D7573",
+    animals: [
+      A.Moonkey,
+      A.LunarTick,
+      A.Tribble,
+      A.Moonicorn,
+      A.LunaMoth,
+      A.JadeRabbit,
+      A.Babmoon,
+    ],
+  },
+  {
+    name: "Mars",
+    backgroundColor: "#9F6659",
+    headerColor: "#D47E6D",
+    animals: [
+      A.Marsmot,
+      A.Marsmoset,
+      A.Rock,
+      A.Rover,
+      A.Martian,
+      A.Marsmallow,
+      A.Marsten,
+    ],
+  },
+  {
+    name: "Constellation",
+    backgroundColor: "#112338",
+    headerColor: "#4E5E6B",
+    animals: [
+      A.Chamaeleon,
+      A.Corvus,
+      A.Lynx,
+      A.Pisces,
+      A.Capricornus,
+      A.Pegasus,
+      A.Horologium,
+    ],
+  },
+];
+
+export const pets: Array<A.Animal> = [
+  A.RabbitPet,
+  A.Bird,
+  A.Dog,
+  A.Cat,
+  A.Fish,
+  A.Turtle,
+  A.Lizard,
+  A.Hamster,
 ];
